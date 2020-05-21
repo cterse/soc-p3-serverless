d7e9dde162d105ef0f9f6fb1ffb961a3829af715
Daria Smirnova <dariaa3004@gmail.com>
2020-05-22 02:04:05 +0930

    Added order handler

---
+++ serverless/logistics/order/handler.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import os
+import uuid
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Orders_')
+
+####################
+def writeToDynamo(event, context):
+    print('Generating new DynamoDB record')
+    
+    orderID = json.loads(event["body"]).get("orderID")
+    address = json.loads(event["body"]).get("address")
+    items = json.loads(event["body"]).get("items")
+    
+    entry = {
+      "orderID": orderID,
+      "address": address,
+      "items": item
+    }
+    
+    #Creating new record in DynamoDB table
+    table.put_item(Item=entry)
+    
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(entry)
+    }
+
+    return response
