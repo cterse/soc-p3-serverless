afa389bb3527d856eeb1ccbbadb8bd18c2bc2fc5
Daria Smirnova <dariaa3004@gmail.com>
2020-05-23 12:23:59 +0000

    Wrapper agent implementation

---
+++ serverless/logistics/wrapper/Wrapper.py
@@ -0,0 +1,36 @@
+import pos
+import json
+import uuid
+
+name = "Wrapper"
+
+with open("logistics.json") as stream:
+    protocol = json.load(stream)
+with open("configuration.yml") as conf:
+    configuration = yaml.safe_load(conf)
+
+adapter = pos.Adapter(name, protocol, configuration, 'WrapperHistory')
+
+@adapter.received(protocol['messages']['RequestWrapping'])
+def handleRequestWrapping(message, enactment):
+    print("RequestWrapping received: " + json.dumps(message))
+
+    if message["item"]=="plate" or message["item"]=="glass":
+        wrapping = "bubblewrap"
+    elif:
+        wrapping = "paper"
+
+    wrapped = {"orderID": message["orderID"], "itemID": message["itemID"], "wrapping": wrapping}
+    ok, msg = adapter.send("Packer", wrapped)
+    if not ok:
+        print(msg)
+
+
+@adapter.sent(protocol['messages']['Wrapped'])
+def handleLabeled(message, enactment):
+    print("Wrapped message has been sent: " + json.dumps(message))
+
+
+
+def lambda_handler(*args):
+    return adapter.handler(*args)
